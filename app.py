c992c8204c5b1e43750786833878a624622210e4
Chris <ccparr13@gmail.com>
2018-05-15 20:29:23 +0100

    revert previous test

--- app.py
+++ app.py
@@ -87,15 +87,16 @@ def setAddress(latchNumber):
 def applyLatchAction(latchNum, action):
 	#setOutput(17, True) #change mode to memory to ignore input
 	setAddress(latchNum)
-	time.sleep(0.01)
-	setOutput(17, False) #change mode to addressable latch
+	#setOutput(17, False) #change mode to addressable latch
 	if action == "off":
-		setOutput(22, False)
+		#setOutput(22, False)
 		setOutput(22, True)
 		#obj_response.alert("Turning latch %s off." % (latchNum))
 	if action == "on":
 		setOutput(22, False)
 		#obj_response.alert("Turning latch %s on." % (latchNum))
+	setOutput(17, False) #change mode to addressable latch
+	time.sleep(0.01)
 	setOutput(17, True) #change mode to memory to ignore input
 
 def setOutput(pinNumber, isHigh):
